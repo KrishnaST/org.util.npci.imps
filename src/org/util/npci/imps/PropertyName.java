93f993b9b8a722f78c83b3ebde266d0ae22459bb
krishna.telgave <krishna.telgave@SIL_DEV076.saas.SILASPDC.com>
2019-08-05 14:46:38 +0530

    added local property names.

---
+++ src/org/util/npci/imps/PropertyName.java
@@ -0,0 +1,10 @@
+package org.util.npci.imps;
+
+public final class PropertyName {
+
+	public static final String CBS_IP                       = "CBS_IP";
+	public static final String RETROFIT_LOGGING_LEVEL       = "RETROFIT_LOGGING_LEVEL";
+	public static final String RETROFIT_READ_TIMEOUT_SEC    = "RETROFIT_READ_TIMEOUT_SEC";
+	public static final String RETROFIT_WRITE_TIMEOUT_SEC   = "RETROFIT_WRITE_TIMEOUT_SEC";
+	public static final String RETROFIT_CONNECT_TIMEOUT_SEC = "RETROFIT_CONNECT_TIMEOUT_SEC";
+}
